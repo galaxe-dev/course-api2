29ef89b644dc41d786456d54468e8c16f991c733
prasadglx <prasadglx9@gmail.com>
2020-08-14 18:59:58 +0530

    initial project setup

---
+++ src/main/java/com/glx/topic/TopicController.java
@@ -0,0 +1,45 @@
+package com.glx.topic;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private Topicservice topicService;
+	@GetMapping("/topics")
+	public List<Topic> getTopics() {
+		return topicService.getAllTopics();
+	}
+	
+	@GetMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+
+	@PostMapping("/topics")
+	public void addtopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+		topicService.updateTopic(id,topic);
+	}
+	
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deletetopic(id);
+	}
+}
